cdc2b0b91752df280b4876465641020f66e695dc
440z <hisdudeness777@web.de>
2021-07-27 15:11:18 +0200

    Add project files.

---
+++ Models/Document.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EditWordWithOpenXML.Models
+{
+    public class Document
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Template { get; set; } = (@"C:\Users\ITA8-TN04\OneDrive - IT-Akademie Dr. Heuer GmbH\git\EditWordWithOpenXML\Data\Vorher.docx");
+    }
+}
