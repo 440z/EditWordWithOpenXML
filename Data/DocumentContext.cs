29e071ad68c390e31015d2c746ee3b2f48b87914
440z <hisdudeness777@web.de>
2021-07-27 15:57:05 +0200

    Word Datei wird schon mal editiert

---
+++ Data/DocumentContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EditWordWithOpenXML.Models;
+
+    public class DocumentContext : DbContext
+    {
+        public DocumentContext (DbContextOptions<DocumentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EditWordWithOpenXML.Models.Document> Document { get; set; }
+    }
--- Models/Document.cs
+++ Models/Document.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Packaging;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace EditWordWithOpenXML.Models
@@ -10,5 +13,26 @@ namespace EditWordWithOpenXML.Models
         public int ID { get; set; }
         public string Name { get; set; }
         public string Template { get; set; } = (@"C:\Users\ITA8-TN04\OneDrive - IT-Akademie Dr. Heuer GmbH\git\EditWordWithOpenXML\Data\Vorher.docx");
+
+        // To search and replace content in a document part.
+        public static void SearchAndReplace(string document)
+        {
+            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(document, true))
+            {
+                string docText = null;
+                using (StreamReader sr = new StreamReader(wordDoc.MainDocumentPart.GetStream()))
+                {
+                    docText = sr.ReadToEnd();
+                }
+
+                Regex regexText = new Regex("auszutschen");
+                docText = regexText.Replace(docText, "Hi Everyone!");
+
+                using (StreamWriter sw = new StreamWriter(wordDoc.MainDocumentPart.GetStream(FileMode.Create)))
+                {
+                    sw.Write(docText);
+                }
+            }
+        }
     }
 }
